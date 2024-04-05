4e33033c9526045884833bc02a189d98a95f5c28
dadanhdyt <dadanhidyt@gmail.com>
2024-04-05 20:19:44 +0700

    fix(orang tua): :bug: Ada eror pada validation
    Karena field nik uniue saya lupa untuk memvalidasi nya. sekarang sudah bisa

--- app/Http/Controllers/Peserta/DataOrangTuaController.php
+++ app/Http/Controllers/Peserta/DataOrangTuaController.php
@@ -24,9 +24,9 @@ class DataOrangTuaController extends Controller
             $allowed = ['ayah', 'ibu', 'wali'];
             if (in_array($type, $allowed)) {
                 $data = $dataOrangTuaService->storeAndValidate([
-                     'pendaftaran_id' => auth()->user()->pendaftaran->id,
-                     'type' => $type,
-                 ]);
+                    'pendaftaran_id' => auth()->user()->pendaftaran->id,
+                    'type' => $type,
+                ]);
 
                 return redirect()->route('peserta.pendaftaran.form.data-orang-tua', [
                     'id' => $data->id,
@@ -36,42 +36,57 @@ class DataOrangTuaController extends Controller
         if (!$id && !$request->get('type')) {
             return $this->listDataOrangTua($dataOrangTuaService);
         } else {
+            $dataOrangTua = $dataOrangTuaService->getById($id);
+            if (!$dataOrangTua) {
+                abort(404);
+            }
+
             return view('peserta.formulir.data-orang-tua-isi', [
-                'data' => $dataOrangTuaService->getById($id),
+                'data' => $dataOrangTua,
             ]);
         }
     }
 
     public function simpan(Request $request, DataOrangTuaService $dataOrangTuaService, $id)
     {
-        $ortu_ids = $request->user()->pendaftaran->orangtua->pluck('id')->toArray();
-        $validated = $request->validate([
+        $orangTua = $request->user()->pendaftaran->orangtua;
+        $validate_rules = [
+            'nik' => 'required|string|size:16',
             'nama' => 'required|string|max:255',
-            'nik' => 'required|string|size:16', // Sesuaikan panjang NIK sesuai aturan
             'pekerjaan' => 'required|string|max:255',
             'penghasilan' => 'required|nullable|numeric', // Bisa diubah menjadi required jika penghasilan wajib diisi
             'pendidikan_terakhir' => 'required|string|max:255',
             'alamat' => 'required|string|max:255',
             'no_telepon' => 'required|string|max:255',
-        ]);
+        ];
+        /**
+         * cek apakah nik yang di ketikan sama dengan nik lama
+         * jika tidak validasi nik nya agar mencegah kesamaan nik dengan orang tua lain
+         */
+        if ($nik = $request->post('nik')) {
+            $originalNIK = $dataOrangTuaService->getById($id)->nik;
+            if ($nik != $originalNIK) {
+                $validate_rules['nik'] .= '|unique:data_orang_tuas,nik';
+            }
+        }
+        $validated = $request->validate($validate_rules);
         try {
             /*
              * cek apakah id nya dimiliki sama peserta
              */
-            if (in_array($id, $ortu_ids)) {
+            if (in_array($id, $orangTua->pluck('id')->toArray())) {
                 // jika ya ambil data dari service lalu update
                 $orangTua = $dataOrangTuaService->getById($id);
                 $orangTua->update($validated);
                 // redirect ke halaman orang tua
                 toast('Data orang tua berhasil di perbaharui', 'success');
-
                 return redirect()->route('peserta.pendaftaran.form.data-orang-tua')->withErrors([
                     'toast' => 'Data berhasil di update',
                 ]);
             }
-            echo 324;
         } catch (\Throwable $th) {
             Log::alert($th->getMessage());
+            abort(500);
         }
     }
 }
